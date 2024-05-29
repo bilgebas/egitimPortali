175edbb0176d3f8ffdc0c81db0a32c111a510c35
Bilge Baş <bilgebassss@gmail.com>
2024-05-29 16:30:57 +0300

    yorum satırı

--- egitimPortali/Models/Education.cs
+++ egitimPortali/Models/Education.cs
@@ -9,6 +9,6 @@
         public int InstructorId { get; set; }
         public int Capacity { get; set; }
         public decimal Cost { get; set; }
-        public int Duration { get; set; }
+        public int Duration { get; set; }//44
     }
 }
